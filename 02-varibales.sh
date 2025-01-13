480b0ebd4f228dd6105f69393a2920bba9766ed8
aleefbasha123 <aleefbasha123@gmail.com>
2025-01-14 00:31:45 +0530

    2nd scrip

--- 02-varibales.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Ramesh: Hello suresh good morning"
-echo "Suresh: uresh Hi ramesh very good morning"
-echo "Ramesh: How are you suresh?"
-echo "Suresh: I am good ramesh, How are you?"
