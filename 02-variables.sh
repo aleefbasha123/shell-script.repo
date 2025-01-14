9bdbc4f69af12b0992bb90bb00ee4307da636471
aleefbasha123 <aleefbasha123@gmail.com>
2025-01-14 13:23:39 +0530

    we donot have any central repo

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1: Hello $PERSON2 good morning"
+echo "$PERSON2: $PERSON2 Hi $PERSON1 very good morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1, How are you?"
