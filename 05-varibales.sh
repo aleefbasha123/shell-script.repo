502bb4bacf5d17db26faef9527874ca9a8b2f549
aleefbasha123 <aleefbasha123@gmail.com>
2025-01-14 14:57:34 +0530

    adding new data

--- 05-varibales.sh
+++ 05-varibales.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 echo "Please enter username"
-read USERNAME #the value entered above will be automatically attached to the variable USERNAME
+read -s USERNAME #ntered above will be automatically attached to the variable USERNAME
 
 
 echo "Username is: $USERNAME"
