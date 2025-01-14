25ed59db4708abb627075853b746ae4509104463
aleefbasha123 <aleefbasha123@gmail.com>
2025-01-14 15:23:57 +0530

    adding new data

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -5,6 +5,3 @@ NUMBER=$1
 NUMBER=$2
 SUM=$(($NUMBER1+$NUMBER2))
 echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM"
-echo "Sum of $NUMBER1 and $NUMBER2 is: $(($NUMBER1+$NUMBER2))"
-echo "Sum of $NUMBER1 and $NUMBER2 is: $((NUMBER1+NUMBER2))"
-echo "Sum of $NUMBER1 and $NUMBER2 is: $((NUMBER1+NUMBER2))"
