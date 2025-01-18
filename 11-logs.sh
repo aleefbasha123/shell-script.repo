13c025d928b991bf6de138c7fcc5b64be3f9323e
aleefbasha123 <aleefbasha123@gmail.com>
2025-01-18 23:16:22 +0530

    conditions

---
+++ 11-logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+echo "script name: $0"
+
+VALIDATE(){
+    ID=$(id -u)
+
+  if  [ $1 -ne 0 ]
+  then
+        echo "ERROR:: $1..........failed"
+
+  else
+      echo "$2-------------- Installaton Success"
+  fi
+}
+
+if  [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+
+else
+    echo "you are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+
+VALIDATE $? "Installing Git"
