b4a3cb63d8a16e552b5d3ee101c3edd19571603d
aleefbasha123 <aleefbasha123@gmail.com>
2025-01-20 12:20:57 +0530

    instlling pakcages

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -6,6 +6,19 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started    exceduing at $TIMESTAMP"
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILED $N"
+    else
+        echo -e "$2...$G Success $N"
+    fi
+}
+
 if [ $ID -ne 0 ]
 then 
     echo -e "$R ERROR:: Please run the script with root access"
@@ -14,4 +27,21 @@ else
     echo "you are in root user"
 fi #fi means reverse of if, including condtion end.
 
-echo "All the arguments passed:$@"
+#echo "All the arguments passed:$@"
+#git mysql postfix net-tools
+#package=git for first time
+
+
+
+for package in $@
+do
+  yun list installed $pacakge #check installed or not
+  if [ $? -ne 0 ] #if not installed
+  then
+      yum install $package -y #install the package
+      VALIDATE $? "Installing of $package" #validate
+      else
+          echo -e "$package is already installed ... $Y SKIPPING $N"
+
+    fi
+done
