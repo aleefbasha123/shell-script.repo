390d9bf55b1ed678e4d32b59f2dedc61a96bc642
aleefbasha123 <aleefbasha123@gmail.com>
2025-01-20 11:32:34 +0530

    instlling pakcages

---
+++ 13-install-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ID=$(ID -U)
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: Please run the script with root access"
+    exit 1 #you can give other than 0
+else
+    echo "you are in root user"
+fi #fi means reverse of if, including condtion end.
+
