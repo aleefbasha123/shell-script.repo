6e6447289694500374800fbd8cd3df43d32ae590
aleefbasha123 <aleefbasha123@gmail.com>
2025-02-26 16:01:08 +0530

    comment

--- 16.disk-usage.sh
+++ 16.disk-usage.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-DISK_THESOULD=5
+DISK_THRESOULD=5
 MESSAGE=""
 
 while IFS= read -r line
 do
-   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
    FOLDER=$(echo $line | awk -F " " '{print $NF}')
-   if [ $USAGE -ge $DISK_THESOULD ]
+   if [ $USAGE -ge $DISK_THRESOULD ]
    then
-       MESSAGE+="$FOLDER is more than $DISK_THESOULD, current usage:$USAGE \n"
+       MESSAGE+="$FOLDER is more than $DISK_THRESOULD, current usage: $USAGE \n"
    fi
 done <<< $DISK_USAGE
 
